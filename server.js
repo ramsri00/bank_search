878a1f1db0cd34e2c2e4a22e75f74d92b51e6db2
ramsri00 <srirammk1998@gmail.com>
2019-12-23 22:36:39 +0530

    second commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app= express();
 app.use(express.static(__dirname + '/dist/banksearch' ));
 app.listen(process.env.PORT || 8080);
 
-app.get('*'),function(req,res){
+app.get('/*'),function(req,res){
     res.sendFile(path.join(__dirname+'dist/banksearch/index.html'));
 }
 
